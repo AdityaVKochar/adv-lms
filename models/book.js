9c713cfdcab336fd2599b110a0c3363759045312
AdityaVKochar <adityavardhankochar@gmail.com>
2025-03-05 20:11:36 +0530

    Working Model

--- models/book.js
+++ models/book.js
@@ -1,6 +1,20 @@
 const mongoose = require('mongoose');
+const uri = process.env.MONGODB_URI;
+const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
 
 mongoose.connect('mongodb://localhost:27017/gdsc-lmsdb');
+
+async function run() {
+    try {
+      // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
+      await mongoose.connect(uri, clientOptions);
+      await mongoose.connection.db.admin().command({ ping: 1 });
+      console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+      ;
+    }
+  }
+  run().catch(console.dir);
 
 const bookSchema = mongoose.Schema({
     book_id: String,
